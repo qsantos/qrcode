d4a25f4869096eaf671e34fda1bb118ce4e22e2e
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-11-11 17:33:26 +0100

    Adding support for P4

--- pbm.c
+++ pbm.c
@@ -50,16 +50,19 @@ static byte nextint(FILE* f)
 	}
 	return res;
 }
+static byte nextbit(FILE* f, byte* buf, size_t* buf_avail)
+{
+	if (!*buf_avail)
+	{
+		fread(buf, 1, 1, f);
+		*buf_avail = 8;
+	}
+	return (*buf >> (--(*buf_avail))) & 1;
+}
 void pbm_file_to_bitmap(bitmap_t* img, FILE* f)
 {
 	fgetc(f); // reads 'P'
 	char format = fgetc(f); // reads format (1-6)
-
-	if (format != '1')
-	{
-		fprintf(stderr, "Unsupported format\n");
-		exit(1);
-	}
 
 	byte w = nextint(f);
 	byte h = nextint(f);
@@ -75,14 +78,38 @@ void pbm_file_to_bitmap(bitmap_t* img, FILE* f)
 	img->height = h;
 	img->data   = d;
 
-	for (size_t i = 0; i < h; i++)
+	if (format == '1')
+	{
+		for (size_t i = 0; i < h; i++)
 		for (size_t j = 0; j < w; j++)
 		{
-			byte v = nextint(f) == 0 ? 0 : 255;
+			byte v = nextint(f) ? 255 : 0;
 			PIX(img, i, j).r = v;
 			PIX(img, i, j).g = v;
 			PIX(img, i, j).b = v;
 		}
+	}
+	else if (format == '4')
+	{
+		byte buf = 0;
+		size_t buf_avail = 0;
+		for (size_t i = 0; i < h; i++)
+		{
+			for (size_t j = 0; j < w; j++)
+			{
+				byte v = nextbit(f, &buf, &buf_avail) ? 255 : 0;
+				PIX(img, i, j).r = v;
+				PIX(img, i, j).g = v;
+				PIX(img, i, j).b = v;
+			}
+			buf_avail = 0;
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Unsupported format '%c'\n", format);
+		exit(1);
+	}
 }
 
 void pbm_del(bitmap_t* img)
