292d132f539072baccc805958d5d161963f9551c
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-11-15 15:02:10 +0100

    Added licence

--- modules.c
+++ modules.c
@@ -1,3 +1,21 @@
+/*\
+ *  Yes, This Is Another Barcode Reader
+ *  Copyright (C) 2013 Quentin SANTOS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\*/
+
 #include "modules.h"
 
 static int  is_data (scanner_t* scanner, size_t i, size_t j);
