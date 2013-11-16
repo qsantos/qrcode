687071d82568e9ae8948369d4ef6698442cbe204
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-11-16 18:12:02 +0100

    Even more comments (in data.h)

--- data.h
+++ data.h
@@ -19,18 +19,35 @@
 #ifndef DATA_H
 #define DATA_H
 
+// 7 by 7 matrix representing the
+// pattern finder found in each corner
 extern const unsigned char pattern_finder[7][7];
 
+// 45-byte long character set for
+// the alphanumeric encoding mode
 extern const char* charset_alpha;
 
+// version to version range translation
 extern const unsigned char version_range[41];
+
+// number of bits used to encode the segment length
 extern const unsigned char lenbits[5][3]; // [encoding][version range]
 
+// format and version information decoding data
 extern const unsigned long bch_format_gen;
 extern const unsigned long bch_format_mask;
 extern const unsigned long bch_version_gen;
 extern const unsigned long bch_version_mask;
 
+// row 4*v+c gives six numbers A,B,C,D,E,F
+// v = version
+// c = error correction level
+// A = number of blocks of the first kind
+// B = number of codewords in each of these blocks
+// C = number of data codewords in each of these blocks
+// D = number of blocks of the second kind
+// E = number of codewords in each of these blocks
+// F = number of data codewords in each of these blocks
 extern const unsigned char block_sizes[160][6];
 
 #endif
