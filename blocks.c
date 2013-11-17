acfe21e8aa4da4586f02a73979e88e66b896d715
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-11-17 11:55:22 +0100

    Writing blocks to image

--- blocks.c
+++ blocks.c
@@ -20,6 +20,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "data.h"
 #include "modules.h"
@@ -32,6 +33,7 @@
 
 
 static void get_block(scanner_t* scanner);
+static void put_block(scanner_t* scanner);
 
 static void get_block(scanner_t* scanner)
 {
@@ -80,14 +82,13 @@ static void get_block(scanner_t* scanner)
 		skip_bits(scanner, (cur-b[0]) * 8);
 		scanner->block_data[n_data-1] = get_codeword(scanner);
 		skip_bits(scanner, (nblocks-cur-1) * 8);
-
 	}
 
 	// END read data
 
 
 
-	// the module pointers is now at the end of all the data and at
+	// the module pointer is now at the end of all the data and at
 	// the beginning of the interleaved error correction codewords
 
 
@@ -147,9 +148,95 @@ unsigned int get_bits(scanner_t* scanner, size_t n)
 	return res;
 }
 
+static void put_block(scanner_t* scanner)
+{
+	// get block information
+	const byte* b = block_sizes[4*scanner->v + scanner->c];
+
+	// current block
+	size_t cur = scanner->block_cur;
+
+	// find the data size of the current block
+	size_t n_data = scanner->block_dataw;
+
+	// rewind to start of symbol
+	scanner->i = scanner->s-1;
+	scanner->j = scanner->s-1;
+
+	// apply Reed-Solomon error correction
+	size_t n_errwords = b[1] - b[2]; // same for both types of blocks
+	rs_encode(n_data, scanner->block_data, n_errwords);
+
+
+	// NOTE: in a symbol, all the blocks have the same number of error
+	//       correction codewords but the first series of blocks can
+	//       have one data codeword less
+
+	byte nblocks = b[0]+ b[3];
+
+
+
+	// BEGIN write data
+	// the next section handles the inverleaving of data codewords
+
+	// handling the minimal number of codewords
+	// n is either n_data-1 (first blocks) or n_data-2 (last ones)
+	for (size_t i = 0; i < b[2]; i++)
+	{
+		skip_bits(scanner, cur * 8);
+		put_codeword(scanner, scanner->block_data[i]);
+		skip_bits(scanner, (nblocks-cur-1) * 8);
+	}
+
+	// interleaving specific to the second series of blocks
+	if (b[2] == n_data) // first kind of block
+	{
+		skip_bits(scanner, b[3] * 8);
+	}
+	else // second kind
+	{
+		skip_bits(scanner, (cur-b[0]) * 8);
+		put_codeword(scanner, scanner->block_data[n_data-1]);
+		skip_bits(scanner, (nblocks-cur-1) * 8);
+	}
+
+	// END write data
+
+
+
+	// the module pointer is now at the end of all the data and at
+	// the beginning of the interleaved error correction codewords
+
+
+
+	// BEGIN write correction
+	// this section handles the interleaving of error correction codewords
+
+	for (size_t i = 0; i < n_errwords; i++)
+	{
+		skip_bits(scanner, cur * 8);
+		put_codeword(scanner, scanner->block_data[n_data+i]);
+		skip_bits(scanner, (nblocks-cur-1) * 8);
+	}
+
+	// END write correction
+}
+
 void put_bits(scanner_t* scanner, size_t n, const char* stream)
 {
-	(void) scanner;
-	(void) n;
-	(void) stream;
+	size_t cur = 0;
+	const byte* b = block_sizes[4*scanner->v + scanner->c];
+	size_t n_data = cur < b[0] ? b[2] : b[5];
+	while (n >= n_data)
+	{
+		scanner->block_cur = cur;
+		scanner->block_dataw = n_data;
+		memcpy(scanner->block_data, stream, n_data);
+		put_block(scanner);
+
+		n -= n_data;
+		stream += n_data;
+		cur++;
+		n_data = cur < b[0] ? b[2] : b[5];
+	}
 }
